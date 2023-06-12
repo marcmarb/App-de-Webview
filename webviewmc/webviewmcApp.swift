c8de77785c1e3ba1c2895b4cbb3d97a969ddcf9a
marcmarb <fotsmacc@gmail.com>
2023-06-12 15:48:26 -0600

    Initial Commit

---
+++ webviewmc/webviewmcApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewmcApp.swift
+//  webviewmc
+//
+//  Created by Marcos on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct webviewmcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
